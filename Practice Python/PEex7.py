10387eca7df9e8fe9dba0e3004aac3248e8968da
Kiwibeing <75240564+Kiwibeing@users.noreply.github.com>
2022-01-04 15:32:16 +0800

    Add files via upload

---
+++ Practice Python/PEex7.py
@@ -0,0 +1,5 @@
+# List comprehension: Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+a = [i for i in a if i % 2 == 0]
+print(a)
